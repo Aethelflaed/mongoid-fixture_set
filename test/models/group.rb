5bc5c48397a6f419b24bfc3b5f5cf8b35516db7c
Geoffroy PLANQUART <geoffroy@planquart.fr>
2015-01-02 17:50:32 +0100

    added group model

---
+++ test/models/group.rb
@@ -0,0 +1,11 @@
+class Group
+  include Mongoid::Document
+  include Mongoid::Timestamps::Created::Short
+
+  field :name
+
+  has_many :main_users, class_name: 'User', inverse_of: :main_group
+
+  has_and_belongs_to_many :users
+end
+
--- test/models/user.rb
+++ test/models/user.rb
@@ -3,5 +3,9 @@ class User
 
   field :firstname
   field :lastname
+
+  belongs_to :main_group, class_name: 'Group', inverse_of: :main_users
+
+  has_and_belongs_to_many :groups
 end
 
